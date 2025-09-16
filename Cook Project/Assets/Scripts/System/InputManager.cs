07ecdce2938ba2fa57a313c5473ab4912bf9f05b
Koa <kelen6107@gmail.com>
2025-09-15 21:08:05 -0400

    added inputmanager to handle action maps, added cooking action map

---
+++ Cook Project/Assets/Scripts/System/InputManager.cs
@@ -0,0 +1,72 @@
+using R3;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoSingleton<InputManager>
+{
+    private string currentMap = "Player";
+    private List<string> actionMapStack = new List<string>();
+    public Subject<string> onActionMapChanged { get; } = new Subject<string>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+        // project-wide input settings solution
+        foreach (var map in InputSystem.actions.actionMaps)
+        {
+            if (map.name is not "Player" or "UI")
+                map.Disable();
+        }
+    }
+
+    public void PushActionMap(string mapName)
+    {
+        actionMapStack.Add(currentMap);
+        SwitchMap(mapName);
+        onActionMapChanged.OnNext(mapName);
+    }
+
+    public void PopActionMap(string mapName)
+    {
+        if (actionMapStack.Count > 0)
+        {
+            actionMapStack.Remove(mapName);
+            string previousMap = actionMapStack.Last();
+            SwitchMap(previousMap);
+            onActionMapChanged.OnNext(previousMap);
+        }
+        else
+        {
+            Debug.LogWarning("ActionMap stack is empty. Cannot revert.");
+        }
+    }
+
+    public void SetActionMap(string mapName)
+    {
+        actionMapStack.Clear();
+        SwitchMap(mapName);
+        onActionMapChanged.OnNext(mapName);
+    }
+
+    public string GetCurrentMap() => currentMap;
+
+    public InputActionMap GetActionMap(string mapName)
+    {
+        var actionMap = InputSystem.actions.FindActionMap(mapName, true);
+        if (actionMap == null)
+        {
+            Debug.LogError($"ActionMap '{mapName}' not found.");
+            return null;
+        }
+        return actionMap;
+    }
+
+    private void SwitchMap(string nextMap)
+    {
+        var asset = InputSystem.actions;
+        asset.FindActionMap(currentMap)?.Disable();
+        asset.FindActionMap(nextMap)?.Enable();
+    }
+}
--- Cook Project/Assets/Scripts/UI/Cooking/CookingUI.cs
+++ Cook Project/Assets/Scripts/UI/Cooking/CookingUI.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CookingUI : MonoBehaviour
 {
     public RecipeSelectionPanel selectionPanel;
+
+    private void Awake()
+    {
+        var actions = InputSystem.actions;
+        actions.FindAction("Esc").performed += ctx => Close();
+    }
+
+    private void OnEnable()
+    {
+        InputManager.Instance.PushActionMap("Cooking");
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.PopActionMap("Cooking");
+    }
 
     public void Open()
     {
