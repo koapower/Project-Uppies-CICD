27e40331f6fec7ff6d136c0add09c9d0fd58255e
koapower <kelen6107@gmail.com>
2025-09-26 10:29:06 -0400

    add the stat for player and a ui
    XD i am yitong
    and i somehow need to install compiler service from nuget

---
+++ Cook Project/Assets/Scripts/UI/PlayerStatUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using R3;
+
+public class PlayerStatUI : MonoBehaviour
+{
+    [SerializeField]
+    private BarItem hpBar;
+    [SerializeField]
+    private BarItem staminaBar;
+
+    private void Start()
+    {
+        var playerStatSystem = PlayerStatSystem.Instance;
+        // HPの変更を監視してUIを更新
+        playerStatSystem.CurrentHP.Subscribe(hp =>
+        {
+            hpBar.UpdateValue(hp, playerStatSystem.MaxHP.Value);
+        }).AddTo(this);
+        playerStatSystem.MaxHP.Subscribe(maxHp =>
+        {
+            hpBar.UpdateValue(playerStatSystem.CurrentHP.Value, maxHp);
+        }).AddTo(this);
+        // Staminaの変更を監視してUIを更新
+        playerStatSystem.CurrentStamina.Subscribe(stamina =>
+        {
+            staminaBar.UpdateValue(stamina, playerStatSystem.MaxStamina.Value);
+        }).AddTo(this);
+        playerStatSystem.MaxStamina.Subscribe(maxStamina =>
+        {
+            staminaBar.UpdateValue(playerStatSystem.CurrentStamina.Value, maxStamina);
+        }).AddTo(this);
+    }
+}
