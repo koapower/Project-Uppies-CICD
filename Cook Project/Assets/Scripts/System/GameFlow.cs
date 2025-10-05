7dbf48e4f46fc7f919ebb95fc45732ea24f7d6f4
Koa <kelen6107@gmail.com>
2025-10-04 21:36:32 -0400

    start the shift after 2 sec into game, trying to fix webgl start shift worldbroadcast not showing problem

--- Cook Project/Assets/Scripts/System/GameFlow.cs
+++ Cook Project/Assets/Scripts/System/GameFlow.cs
@@ -1,12 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
 public class GameFlow : MonoSingleton<GameFlow>
 {
     protected override void Awake()
     {
-        StartGame();
+        StartCoroutine(StartGameSeconds(2));
     }
 
     private void StartGame()
     {
         ShiftSystem.Instance.StartGame();
+    }
+
+    private IEnumerator StartGameSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        StartGame();
     }
 }
